82bfc81e5febcc88722c82d544a92d359643918b
huy <phamtranhuy998@gmail.com>
2021-10-10 21:23:18 +0700

    add audio custom hook, add play music to header

---
+++ src/CustomHook/Audio/Audio.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react'
+
+const useAudio = url => {
+    const [audio] = useState(new Audio(url));
+    const [playing, setPlaying] = useState(false);
+    const startAudio = () => {
+        audio.currentTime = 0;
+        audio.play();
+    }
+    useEffect(() => {
+        playing ? audio.play() : audio.pause();
+      },
+      [playing]
+    );
+  
+    useEffect(() => {
+        audio.addEventListener('ended', startAudio);
+        return () => {
+          audio.removeEventListener('ended', startAudio);
+        };
+      }, []);
+    
+      return [setPlaying];
+}
+
+export default useAudio
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import useAudio from '../../CustomHook/Audio/Audio'
 import Navigator from './Navigator/Navigator'
 import './Header.scss'
-import UnMusic from './icon/un-music.png'
-import Logo from './icon/logo.png'
-import User from './icon/user.png'
+import UnMusic from './assets/icon/un-music.png'
+import Music from './assets/icon/music.png'
+import Logo from './assets/icon/logo.png'
+import User from './assets/icon/user.png'
+
+const WAVE_SOUND = 'https://genshin.mihoyo.com/_nuxt/medias/video-bgm.d8637316.mp3';
+const MUSIC = 'https://genshin.mihoyo.com/_nuxt/medias/video-play.06ec9738.mp3';
 
 function Header() {
+    const [playMusic, setPlayMusic] = useState(false);
+    const [setPlayingWaveSound] = useAudio(WAVE_SOUND);
+    const [setPlayingMusic] = useAudio(MUSIC);
+
+    const handleMusicClicked = () => {
+        setPlayMusic((playMusic) => !playMusic);
+    }
+    const toggleMusic = () => {
+        if (playMusic) {
+            setPlayingMusic(true);
+            setPlayingWaveSound(true);
+        } else {
+            setPlayingMusic(false);
+            setPlayingWaveSound(false);
+        }
+    }
+    useEffect(() => {
+        toggleMusic();
+    }, [playMusic])
+
     return (
         <header className="header">
             <div className="music-box">
-                <img src={UnMusic} alt="un-music" />
+                <img src={playMusic ? Music : UnMusic} alt="music" onClick={handleMusicClicked}/>
             </div>
             <div className="logo">
                 <img src={Logo} alt="logo" />
