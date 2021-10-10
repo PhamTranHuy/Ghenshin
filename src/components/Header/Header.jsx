adab6db57c0f93f54dcc1682d7d29642f2100ced
huy <phamtranhuy998@gmail.com>
2021-10-10 14:45:29 +0700

    create nav component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import Navigator from './Navigator/Navigator'
+import './Header.scss'
 
 function Header() {
     return (
-        <div>
-            aaaa
-        </div>
+        <header className="header">
+            <div>music</div>
+            <div>logo</div>
+            <Navigator />
+            <div>Log in</div>
+            <div>Dowload</div>
+        </header>
     )
 }
 
---
+++ src/components/Header/Navigator/Navigator.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navigator.scss'
+function Navigator() {
+    return (
+        <nav className="header_nav">
+            <div className="nav-button">HOME</div>
+            <div className="nav-button">NEWS</div>
+            <div className="nav-button">CHARACTERS</div>
+            <div className="nav-button">EXPLORE</div>
+            <div className="nav-button">HoYoLAB</div>
+            <div className="nav-button">TOP-UP</div>
+            <div className="nav-button">REDEEM CODE</div>
+        </nav>
+    )
+}
+
+export default Navigator
