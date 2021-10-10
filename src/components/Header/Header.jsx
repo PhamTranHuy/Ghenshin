8bd0e5b045460d3c73c8920fde620491724edf58
huy <phamtranhuy998@gmail.com>
2021-10-10 19:51:25 +0700

    styling header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import Navigator from './Navigator/Navigator'
 import './Header.scss'
+import UnMusic from './icon/un-music.png'
+import Logo from './icon/logo.png'
+import User from './icon/user.png'
 
 function Header() {
     return (
         <header className="header">
-            <div>music</div>
-            <div>logo</div>
+            <div className="music-box">
+                <img src={UnMusic} alt="un-music" />
+            </div>
+            <div className="logo">
+                <img src={Logo} alt="logo" />
+            </div>
             <Navigator />
-            <div>Log in</div>
-            <div>Dowload</div>
+            <div className="space"></div>
+            <div className="login">
+                <div>Log In</div>
+                <div className="icon"><img src={User} alt="logo" /></div>
+            </div>
+            <a href="" className="download">
+                <div>Download</div>
+            </a>
         </header>
     )
 }
