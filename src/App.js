bf3cc28fee56061893712074d9bd703e88f79e12
huy <phamtranhuy998@gmail.com>
2021-10-11 15:18:45 +0700

    add Home Page component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from "./components/Header/Header";
+import HomePage from "./components/HomePage/HomePage";
 function App() {
   return (
     <div className="App">
       <Header />
+      <HomePage />
     </div>
   );
 }
---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './HomePage.scss'
+
+function HomePage() {
+    return (
+        <div className="home-page">
+            <div className="poster"></div>
+        </div>
+    )
+}
+
+export default HomePage
