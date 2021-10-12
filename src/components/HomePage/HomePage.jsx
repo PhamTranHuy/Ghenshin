81a4e885cf86c1f3ae7c89a68e4c1a6e6a4833e2
huy <phamtranhuy998@gmail.com>
2021-10-12 16:14:40 +0700

    fix video home's background attribute

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -10,7 +10,7 @@ function HomePage() {
         <div className="home">
             <div className="home_poster">
                 <div className="background-wrapper">
-                    <video autoplay="autoplay" muted="true" loop>
+                    <video autoPlay={true} muted={true} loop>
                         <source src="https://genshin.mihoyo.com/_nuxt/videos/3e78e80.mp4" type="audio/mp4"/>
                     </video>
                 </div>
