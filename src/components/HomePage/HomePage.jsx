7902cbe5d13e5a250655b791a8c007c5cee00f12
huy <phamtranhuy998@gmail.com>
2021-10-12 13:43:15 +0700

    add poster sign, styling poster video button

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './HomePage.scss'
+import play_icon from './assets/poster/play.png' 
 
 function HomePage() {
     return (
@@ -9,6 +10,14 @@ function HomePage() {
                     <video autoplay="autoplay" muted="true" loop>
                         <source src="https://genshin.mihoyo.com/_nuxt/videos/3e78e80.mp4" type="audio/mp4"/>
                     </video>
+                </div>
+                <div className="poster-sign">
+                    <div className="play-button-wrapper">
+                        <button className="play-button"></button>
+                    </div>
+                    <div>text</div>
+                    <div>download banner</div>
+                    <div>poster arrow</div>
                 </div>
             </div>
         </div>
