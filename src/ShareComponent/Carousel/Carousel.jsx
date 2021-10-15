a796f1247cf0cf6327815151d4d54e8953f9622b
huy <phamtranhuy998@gmail.com>
2021-10-15 22:11:20 +0700

    add switch items method  to carousel

--- src/ShareComponent/Carousel/Carousel.jsx
+++ src/ShareComponent/Carousel/Carousel.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import styles from './Carousel.module.scss'
 
-function Carousel({children}) {
+function Carousel({children, width}) {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [translate, setTranslate] = useState(0);
+    
+    const handleSwitchButtonClicked = (index) => {
+        setActiveIndex(index);
+        setTranslate(-(width * index));
+    }
     return (
-        <div className={styles['container']}>
+        <div className={styles['container']} style={{width: `${width}px` }}>
             <div className={styles['carousel-outer']}>
-                <div className={styles['carousel-inner']}>
+                <div className={styles['carousel-inner']} style={{transform: `translateX(${translate}px)`}}>
                     {children.map((item, i) => (
                         <div key={i} className={styles['item']}>{item}</div>
                     ))}
                 </div>
+            </div>
+            <div className={styles['controls']}>
+                    {children.map((item, i) => (
+                        <div key={i} 
+                            className={
+                                `${styles['button']} ${activeIndex === i ? styles['is-active'] : ''}`
+                            } 
+                            onClick={() => { handleSwitchButtonClicked(i) }}/>
+                    ))}
             </div>
         </div>
     )
@@ -20,8 +36,11 @@ Carousel.propTypes = {
     children: PropTypes.oneOfType([
         PropTypes.arrayOf(PropTypes.node),
         PropTypes.node
-        ]).isRequired
+        ]).isRequired,
+    width: PropTypes.string
 }
-
+Carousel.defaultProps = {
+    width: '640'
+  };
 export default Carousel
 
