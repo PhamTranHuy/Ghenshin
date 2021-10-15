8b8c934dd684b5c2435ebea651455075ad0d7b2c
huy <phamtranhuy998@gmail.com>
2021-10-15 14:51:59 +0700

    add carousel to news-section

--- src/ShareComponent/Carousel/Carousel.jsx
+++ src/ShareComponent/Carousel/Carousel.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styles from './Carousel.module.scss'
 
-function Carousel(props) {
+function Carousel({children}) {
     return (
-        <div>
-            
+        <div className={styles['container']}>
+            <div className={styles['carousel-outer']}>
+                <div className={styles['carousel-inner']}>
+                    {children.map((item, i) => (
+                        <div key={i} className={styles['item']}>{item}</div>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
 
 Carousel.propTypes = {
-
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node
+        ]).isRequired
 }
 
 export default Carousel
--- src/components/HomePage/NewsSection/NewsSection.jsx
+++ src/components/HomePage/NewsSection/NewsSection.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
+import Carousel from '../../../ShareComponent/Carousel/Carousel'
 import './NewsSection.scss'
+import news_img_1 from '../assets/news/news-img-1.jpg'
+import news_img_2 from '../assets/news/news-img-2.jpg'
+import news_img_3 from '../assets/news/news-img-3.jpg'
+import news_img_4 from '../assets/news/news-img-4.jpg'
+const CAROUSEL_ITEMS = [
+    {
+        url: '#',
+        img: news_img_1
+    },  {
+        url: '#',
+        img: news_img_2
+    },  {
+        url: '#',
+        img: news_img_3
+    },  {
+        url: '#',
+        img: news_img_4
+    }
+]
 function NewsSection() {
     return (
         <div className="home_news-section">
             <div className="news-wrapper">
-                <div className="carousel-wrapper">carousel</div>
+                <div className="carousel-wrapper">
+                    <Carousel>
+                        {CAROUSEL_ITEMS.map((item, i) => (
+                            <a key={i} href={item.url}>
+                                <img src={item.img}/>
+                            </a>
+                        ))}
+                    </Carousel>
+                </div>
                 <div className="news-wrapper">news</div>
             </div>
         </div>
