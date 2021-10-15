b4e629d99411f55872a49df409c5d10ddc89f50c
huy <phamtranhuy998@gmail.com>
2021-10-15 14:15:16 +0700

    add news-section into home page,  create carousel component

---
+++ src/ShareComponent/Carousel/Carousel.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Carousel(props) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+Carousel.propTypes = {
+
+}
+
+export default Carousel
+
---
+++ src/components/HomePage/NewsSection/NewsSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './NewsSection.scss'
+function NewsSection() {
+    return (
+        <div className="home_news-section">
+            <div className="news-wrapper">
+                <div className="carousel-wrapper">carousel</div>
+                <div className="news-wrapper">news</div>
+            </div>
+        </div>
+    )
+}
+
+export default NewsSection
