5373831d367281b2978b9d046c9ec5880bd8be0c
huy <phamtranhuy998@gmail.com>
2021-11-10 23:51:50 +0700

    update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,8 @@
 import './Footer.scss';
+import miHoYo_logo from './assets/miHoYo-logo.png'
+import genshin_logo from './assets/genshin-logo.png'
+import europe from './assets/europe.png'
+import us_canada from './assets/us-canada.png'
 
 function Footer() {
     return (
@@ -33,7 +37,34 @@ function Footer() {
                     <div className="arrow"></div>
                 </div>
             </div>
-            <div className="footer-info"></div>
+            <div className="footer-info">
+                <div className="logo-wrapper">
+                    <img src={miHoYo_logo} alt="" />
+                    <img src={genshin_logo} alt="" />
+                </div>
+                <div className="content-wrapper">
+                    <div className="links">
+                        <a href="" className="link">Privacy Policy</a>
+                        <a href="" className="link">Terms of Service</a>
+                        <a href="" className="link">About Us</a>
+                        <a href="" className="link">Contact Us</a>
+                    </div>
+                    <div className="copyright">
+                        Copyright © 2020 miHoYo All Rights Reserved
+                    </div>
+                    <div className="extra-info">
+                        "PlayStation Family Mark," "PS5 logo" and "PS4 logo" are registered trademarks or trademarks of Sony Interactive Entertainment Inc.
+                    </div>
+                    <div className="content">
+                        Epic, Epic Games, Epic Games Store, the Epic Games Store logo, and Epic Online Services are trademarks and/or registered trademarks of Epic Games. 
+                        All other trademarks are the property of their respective owners.
+                    </div>
+                    <div className="logos">
+                        <img src={europe} alt="" />
+                        <img src={us_canada} alt="" />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
