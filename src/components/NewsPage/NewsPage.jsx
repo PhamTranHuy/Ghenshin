24aa27c3801f93900f7ee6101f791f5c69291a2f
huy <phamtranhuy998@gmail.com>
2022-02-16 22:46:27 +0700

    add scroll to top button into news page

--- src/components/NewsPage/NewsPage.jsx
+++ src/components/NewsPage/NewsPage.jsx
@@ -1,12 +1,22 @@
+import "./NewsPage.scss"
 import Footer from "../Footer/Footer"
 import Header from "../Header/Header"
 import Recommend from "./Recommend/Recommend"
 import News from "./News/News"
 import GET_NEWS_INFO_API from "./data";
 import { useState, useEffect } from "react";
+import useWindowScroll from "../../CustomHook/WindowScroll"
+import clsx from "clsx"
 
 function NewsPage() {
     const [newsInfo, setNewsInfo] = useState([]);
+    const [showScrollToTopButton, setShowScrollToTopButton] = useState(false);
+    const windowScroll = useWindowScroll();
+
+    const handleScrollToTopClicked = () => {
+        window.scrollTo(windowScroll.scrollX, 0);
+    }
+
     useEffect(() => {
         const fetchNewsInfo = async () => {
             const newsInfo = await GET_NEWS_INFO_API();
@@ -14,12 +24,20 @@ function NewsPage() {
         }
         fetchNewsInfo();
     }, [])
+    useEffect(() => {
+        if (windowScroll.scrollY >= 550) {
+            setShowScrollToTopButton(true);
+        } else {
+            setShowScrollToTopButton(false);
+        }
+    }, [windowScroll])
     return (
-        <div>
+        <div className="news-page">
             <Header />
             <Recommend newsInfo={newsInfo}/>
             <News newsInfo={newsInfo}/>
             <Footer />
+            <div className={clsx("scroll-to-top", showScrollToTopButton && "show")} onClick={handleScrollToTopClicked}></div>
         </div>
     )
 }
