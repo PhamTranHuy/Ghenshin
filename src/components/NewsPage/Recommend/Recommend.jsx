19950204a7165eb047095fa6188153d9170a0b8a
huy <phamtranhuy998@gmail.com>
2022-02-26 19:06:45 +0700

    add condition show/hide carousel, resize carousel

--- src/components/NewsPage/Recommend/Recommend.jsx
+++ src/components/NewsPage/Recommend/Recommend.jsx
@@ -1,27 +1,33 @@
 import Carousel from "../../../ShareComponent/Carousel/Carousel"
 import "../Recommend/Recommend.scss"
 import NewsBlock from "./NewsBlock/NewsBlock"
+import useWindowSize from "../../../CustomHook/WindowSize"
 
 function Recommend({newsInfo}) {
+    const windowSize = useWindowSize();
     return (
         <div className="recommend">
             <div className="wrapper">
-                <div className="news-block-container" style={{display: 'none'}}>
-                    {newsInfo.filter((item, index) => index <= 2)
-                        .map((item, index) => 
-                        <NewsBlock newsInfo={item} key={index} />
-                    )}
-                </div>
-                <div className="news-block-carousel">
-                    <Carousel width={window.innerWidth}>
+                {
+                    windowSize.width > 1200 && <div className="news-block-container">
                         {newsInfo.filter((item, index) => index <= 2)
                             .map((item, index) => 
-                            <div className="item-wrapper" style={{width: window.innerWidth}}>
-                                <NewsBlock newsInfo={item} key={index} />
-                            </div>
+                            <NewsBlock newsInfo={item} key={index} />
                         )}
-                    </Carousel>
-                </div>
+                    </div>
+                } 
+                { 
+                    windowSize.width <= 1200 && <div className="news-block-carousel">
+                        <Carousel width={windowSize.width}>
+                            {newsInfo.filter((item, index) => index <= 2)
+                                .map((item, index) => 
+                                <div key={index} className="item-wrapper" style={{width: windowSize.width}}>
+                                    <NewsBlock newsInfo={item} key={index} />
+                                </div>
+                            )}
+                        </Carousel>
+                    </div>
+                }
             </div>
         </div>
     )
