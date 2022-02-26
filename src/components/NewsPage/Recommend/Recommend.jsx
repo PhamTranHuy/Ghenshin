f7e34c982f7e46e89ed8460d2092279e82ba3662
huy <phamtranhuy998@gmail.com>
2022-02-26 14:26:40 +0700

    add carousel into mobile views news page

--- src/components/NewsPage/Recommend/Recommend.jsx
+++ src/components/NewsPage/Recommend/Recommend.jsx
@@ -1,3 +1,4 @@
+import Carousel from "../../../ShareComponent/Carousel/Carousel"
 import "../Recommend/Recommend.scss"
 import NewsBlock from "./NewsBlock/NewsBlock"
 
@@ -5,11 +6,21 @@ function Recommend({newsInfo}) {
     return (
         <div className="recommend">
             <div className="wrapper">
-                <div className="news-block-container">
+                <div className="news-block-container" style={{display: 'none'}}>
                     {newsInfo.filter((item, index) => index <= 2)
                         .map((item, index) => 
                         <NewsBlock newsInfo={item} key={index} />
                     )}
+                </div>
+                <div className="news-block-carousel">
+                    <Carousel width={window.innerWidth}>
+                        {newsInfo.filter((item, index) => index <= 2)
+                            .map((item, index) => 
+                            <div className="item-wrapper" style={{width: window.innerWidth}}>
+                                <NewsBlock newsInfo={item} key={index} />
+                            </div>
+                        )}
+                    </Carousel>
                 </div>
             </div>
         </div>
