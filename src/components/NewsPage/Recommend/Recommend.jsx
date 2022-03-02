41c9df50a63f28d99f33691d7ad73ee11e6a86f9
huy <phamtranhuy998@gmail.com>
2022-03-02 22:13:29 +0700

    update responsive for news page

--- src/components/NewsPage/Recommend/Recommend.jsx
+++ src/components/NewsPage/Recommend/Recommend.jsx
@@ -2,9 +2,14 @@ import Carousel from "../../../ShareComponent/Carousel/Carousel"
 import "../Recommend/Recommend.scss"
 import NewsBlock from "./NewsBlock/NewsBlock"
 import useWindowSize from "../../../CustomHook/WindowSize"
+import { useState, useLayoutEffect } from "react";
 
 function Recommend({newsInfo}) {
     const windowSize = useWindowSize();
+    const [carouselWidth, setCarouselWidth] = useState();
+    useLayoutEffect(() => {
+        setCarouselWidth(document.body.offsetWidth);
+    }, [windowSize.width])
     return (
         <div className="recommend">
             <div className="wrapper">
@@ -18,10 +23,10 @@ function Recommend({newsInfo}) {
                 } 
                 { 
                     windowSize.width <= 1200 && <div className="news-block-carousel">
-                        <Carousel width={windowSize.width}>
+                        <Carousel width={carouselWidth}>
                             {newsInfo.filter((item, index) => index <= 2)
                                 .map((item, index) => 
-                                <div key={index} className="item-wrapper" style={{width: windowSize.width}}>
+                                <div key={index} className="item-wrapper" style={{width: carouselWidth}}>
                                     <NewsBlock newsInfo={item} key={index} />
                                 </div>
                             )}
