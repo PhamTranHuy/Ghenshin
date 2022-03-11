ab2e62a5d4d007d9eefeb97dcfa93ca650322f83
huy <phamtranhuy998@gmail.com>
2022-03-11 18:29:27 +0700

    add character avatars data

---
+++ src/components/Characters/assets/data.js
@@ -0,0 +1,33 @@
+const CHARACTER_AVATAR_URL = "src/component/Characters/assets/character-avatar"
+export const CHARACTER_IMG_LIST = [
+    {
+        id: 0,
+        name: "Jean",
+        city: "mondstadt",
+        img_url: `${CHARACTER_AVATAR_URL}/mondstadt/jean`
+    },
+    {
+        id: 1,
+        name: "Amber",
+        city: "mondstadt",
+        img_url: `${CHARACTER_AVATAR_URL}/mondstadt/amber`
+    },
+    {
+        id: 2,
+        name: "Lisa",
+        city: "mondstadt",
+        img_url: `${CHARACTER_AVATAR_URL}/mondstadt/lisa`
+    },
+    {
+        id: 3,
+        name: "Kaeya",
+        city: "mondstadt",
+        img_url: `${CHARACTER_AVATAR_URL}/mondstadt/keaya`
+    },
+    {
+        id: 4,
+        name: "Barbara",
+        city: "mondstadt",
+        img_url: `${CHARACTER_AVATAR_URL}/mondstadt/barbara`
+    },
+]
