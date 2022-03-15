0ead7f723a6ccee8217cdf39c7aae10f0b480bca
huy <phamtranhuy998@gmail.com>
2022-03-15 21:49:09 +0700

    add characterDetail background

--- src/components/Characters/CharacterContent/CharacterContent.jsx
+++ src/components/Characters/CharacterContent/CharacterContent.jsx
@@ -1,12 +1,14 @@
 import "./CharacterContent.scss"
 import AnimateBackground from "./AnimateBackground/AnimateBackground"
 import CharacterSlider from "./CharacterSlider/CharacterSlider"
+import CharacterDetail from "./CharacterDetail/CharacterDetail"
 
 function CharacterView({ background, characterAvatars}) {
     return (
         <div className="character-content-wrapper">
             <AnimateBackground background={background}/>
             <CharacterSlider characterAvatars={characterAvatars} />
+            <CharacterDetail />
         </div>
     )
 }
