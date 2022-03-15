545a60c02f1887a8b7a472fdbd2865fd52b15dab
huy <phamtranhuy998@gmail.com>
2022-03-15 22:25:26 +0700

    update styles

--- src/components/Characters/CharacterContent/CharacterDetail/CharacterDetail.jsx
+++ src/components/Characters/CharacterContent/CharacterDetail/CharacterDetail.jsx
@@ -6,7 +6,7 @@ function CharacterDetail() {
         <div className="character-detail">
             <img className="character-img" src={CHARACTER_DETAIL_LIST[0].img} alt="" />
             <img className="character-conversation" src={CHARACTER_DETAIL_LIST[0].conversation_img} alt="" />
-            <img className="character-quality" src="" alt="" />
+            <img className="character-quality" src={CHARACTER_DETAIL_LIST[0].quality_img} alt="" />
             <div className="character-info">
                 <h1 className="name">name</h1>
                 <div className="cv"></div>
