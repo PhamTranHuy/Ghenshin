454f7ee50fd57cc49428575cf9c354da997b2245
huy <phamtranhuy998@gmail.com>
2022-03-16 22:12:30 +0700

    add handle content  resize

--- src/ShareComponent/CustomScrollBar/CustomScrollBar.jsx
+++ src/ShareComponent/CustomScrollBar/CustomScrollBar.jsx
@@ -1,9 +1,41 @@
-function CustomScrollBar() {
+import { useState, useEffect, useRef } from "react"
+import "./CustomScrollbar.scss"
+
+function CustomScrollbar({children}) {
+    const contentRef = useRef(null);
+    const scrollThumbRef = useRef(null);
+    const scrollTrackRef = useRef(null);
+    const observable = useRef(null);
+    const [thumbHeight, setThumbHeight] = useState();
+
+    const handleResize = (contentRef, scrollTrackRef) => {
+        const { clientHeight, scrollHeight } = contentRef.current;
+        const {clientHeight: trackSize} = scrollTrackRef.current;
+        const thumbHeight = (clientHeight / scrollHeight) * trackSize;
+        setThumbHeight(thumbHeight);
+    }
+    useEffect(() => {
+        if (contentRef.current && scrollTrackRef.current) {
+            observable.current = new ResizeObserver(() => {
+                handleResize(contentRef, scrollTrackRef);
+            });
+            observable.current.observe(contentRef)
+            return () => {
+                observable.current?.unobserve()
+            }
+        }
+    }, [])
     return (
-        <div>
-            
+        <div className="custom-scrollbar_container">
+            <div className="content" ref={contentRef}>
+                {children}
+            </div>
+            <div className="scrollbar">
+                <div className="track"></div>
+                <div className="thumb" style={{height: `${thumbHeight}px`}}></div>
+            </div>
         </div>
     )
 }
 
-export default CustomScrollBar
+export default CustomScrollbar
