c8b0d81d08145afb9000ffc14d0e9ea37359c7a9
huy <phamtranhuy998@gmail.com>
2022-03-21 17:24:13 +0700

    toggle mobile navigator

--- src/components/Characters/Navigator/Navigator.jsx
+++ src/components/Characters/Navigator/Navigator.jsx
@@ -1,32 +1,69 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useLocation} from "react-router-dom"
+import { useState } from "react"
 import "./Navigator.scss"
 
 function Navigator() {
+    const { pathname } = useLocation();
+    const [toggleNavigator, setToggleNavigator] = useState(false);
+    const getCurrentCity = (pathname) => {
+        const city = pathname.substring(pathname.lastIndexOf('/') + 1);
+        return city;
+    }
+    const toggle = () => {
+        setToggleNavigator((prevState) => !prevState)
+    }
     return (
-        <div className="character-navigator">
-            <ul className="navigator-wrapper">
-                <li className="navigate-button">
-                    <NavLink to="./mondstadt">
-                        <p>Mondstadt</p>
-                    </NavLink>
-                </li>
-                <li className="navigate-button">
-                    <NavLink to="./liyue">
-                        <p>Liyue</p>
-                    </NavLink>
-                </li>
-                <li className="navigate-button">
-                    <NavLink to="./inazuma">
-                        <p>Inazuma</p>
-                    </NavLink>
-                </li>
-                <li className="navigate-button">
-                    <a>
-                        <p>Coming soon</p>
-                    </a>
-                </li>
-            </ul>
-        </div>
+        <>
+            <div className="character-navigator">
+                <ul className="navigator-wrapper">
+                    <li className="navigate-button">
+                        <NavLink to="./mondstadt">
+                            <p>Mondstadt</p>
+                        </NavLink>
+                    </li>
+                    <li className="navigate-button">
+                        <NavLink to="./liyue">
+                            <p>Liyue</p>
+                        </NavLink>
+                    </li>
+                    <li className="navigate-button">
+                        <NavLink to="./inazuma">
+                            <p>Inazuma</p>
+                        </NavLink>
+                    </li>
+                    <li className="navigate-button">
+                        <a>
+                            <p>Coming soon</p>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div className="character-navigator_mobile">
+                <div onClick={toggle} className="navigator-current-city">{getCurrentCity(pathname)}</div>
+                { toggleNavigator && (<ul className="navigator-wrapper">
+                    <li className="navigate-button">
+                        <NavLink to="./mondstadt">
+                            <p>Mondstadt</p>
+                        </NavLink>
+                    </li>
+                    <li className="navigate-button">
+                        <NavLink to="./liyue">
+                            <p>Liyue</p>
+                        </NavLink>
+                    </li>
+                    <li className="navigate-button">
+                        <NavLink to="./inazuma">
+                            <p>Inazuma</p>
+                        </NavLink>
+                    </li>
+                    <li className="navigate-button">
+                        <a>
+                            <p>Coming soon</p>
+                        </a>
+                    </li>
+                </ul>)}
+            </div>
+        </>
     )
 }
 
