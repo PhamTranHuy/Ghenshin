45591e2b7653f110509bdf3ce0eb51c699c04c0f
huy <phamtranhuy998@gmail.com>
2022-03-24 21:06:02 +0700

    load only one character image for desktop or mobile view

--- src/components/Characters/CharacterContent/CharacterDetail/CharacterDetail.jsx
+++ src/components/Characters/CharacterContent/CharacterDetail/CharacterDetail.jsx
@@ -2,12 +2,26 @@ import "./CharacterDetail.scss"
 import whiteDiamond from "../../assets/detail/white-diamond.png";
 import CustomScrollbar from "../../../../ShareComponent/CustomScrollbar/CustomScrollbar";
 import CharacterCv from "./CharacterCv/CharacterCv";
+import useWindowSize from "../../../../CustomHook/WindowSize"
+import { useState, useEffect } from "react";
+
 function CharacterDetail({ character }) {
-    
+    const windowSize = useWindowSize();
+    const [ mobileView, setMobileView ] = useState(false);
+    useEffect(() => {
+        if (windowSize.width <= 1200) {
+            setMobileView(true);
+        } else {
+            setMobileView(false);
+        }
+    }, [windowSize])
     return character && (
         <div className="character-detail" key={character.name}>
-            <img className="character-img desktop" src={character.img} alt="" />
-            <img className="character-img mobile" src={character.mobile_img} alt="" />
+            {
+                !mobileView ? 
+                <img className="character-img desktop" src={character.img} alt="" /> : 
+                <img className="character-img mobile" src={character.mobile_img} alt="" />
+            }
             <img className="character-conversation" src={character.conversation_img} alt="" />
             <img className="character-quality" src={character.quality_img} alt="" />
             <div className="character-info">
