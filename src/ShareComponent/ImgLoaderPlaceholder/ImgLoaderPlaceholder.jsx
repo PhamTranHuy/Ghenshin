800f91920edaa5cbe09c8806a3436105030fa805
huy <phamtranhuy998@gmail.com>
2022-03-26 20:22:51 +0700

    placeholder loader active during source change, update styles

--- src/ShareComponent/ImgLoaderPlaceholder/ImgLoaderPlaceholder.jsx
+++ src/ShareComponent/ImgLoaderPlaceholder/ImgLoaderPlaceholder.jsx
@@ -1,20 +1,27 @@
 import clsx from "clsx";
 import { useState } from "react"
+import { useEffect } from "react/cjs/react.development";
 import "./ImgLoaderPlaceholder.scss"
 
-function ImgLoaderPlaceholder({className, style, src, alt, lazy = false, placeholderSize={width: '100%', height: '100%'}}) {
+function ImgLoaderPlaceholder({className, style, src, alt, lazy = false, placeholderSize={}}) {
     const [loaded, setLoaded] = useState(false);
     const handleLoaded = () => {
-        setLoaded(false);
+        setLoaded(true);
     }
+    useEffect(() => {
+        setLoaded(false);
+    }, [src]);
     return (
-        <div style={style} className={clsx("img-loader-placeholder", className)}>
-            {!loaded && <div className="loader" style={placeholderSize}>
-                <div class="lds-roller">
+        <div className="img-loader-placeholder">
+            {!loaded && <div className={clsx("loader",  className)} style={placeholderSize}>
+                <div className="lds-roller">
                     <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
                 </div>
             </div>}
-            <img src={src} alt={alt} onLoad={handleLoaded} style={{display: loaded ? 'block' : 'none'}}/>
+            <img src={src} alt={alt} 
+                onLoad={handleLoaded} 
+                style={{...style, display: loaded ? 'block' : 'none'}}
+                className={className} />
         </div>
     )
 }
