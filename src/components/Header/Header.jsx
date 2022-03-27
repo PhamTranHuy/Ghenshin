eaf8713e11e49e74ceb9274fefd208fd1984083a
huy <phamtranhuy998@gmail.com>
2022-03-27 19:59:07 +0700

    add link to header-buttons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, NavLink } from 'react-router-dom'
 
 import Navigator from './Navigator/Navigator'
 import Popup from '../../ShareComponent/Popup/Popup'
@@ -50,6 +50,7 @@ function Header() {
                 <img src={playMusic ? Music : UnMusic} alt="music" onClick={toggleMusic}/>
             </div>
             <div className="logo">
+                <NavLink to="/home" className="logo-nav-link"></NavLink>
                 <img src={Logo} alt="logo" />
             </div>
             <Navigator />
